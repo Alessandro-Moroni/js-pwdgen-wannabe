846169e1da80901e0be51c56013065c4eef7220c
Alessandro-Moroni <alemoro98@gmail.com>
2024-02-19 14:31:05 +0100

    [fix]javascript

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 const name = prompt('Come ti chiami?');
 
 
-const username = prompt("Qual'è il tuo cognome?");
+const surname = prompt("Qual'è il tuo cognome?");
 
 
 const color = prompt("Qual'è il tuo colore preferito?");
-console.log(name, surname, color);
 
-document.getElementById('user').innerHTML `
-<p>
- Mi chiamo ${name, '',surname} e il mio colore preferito è il ${color} 
-</p>
+const nameSurname = name + ' ' + surname;
+
+document.getElementById('user').innerHTML = 
 `
+<p>
+ Mi chiamo <strong>${nameSurname}</strong> e il mio colore preferito è il <strong>${color}</strong>
+</p>
+`;
